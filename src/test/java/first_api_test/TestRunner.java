759bedcfcfad0f43e65a5917e2d8f3b028cf0f3a
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2023-05-09 08:58:02 -0500

    Sección 3.1: First API test

---
+++ src/test/java/first_api_test/TestRunner.java
@@ -0,0 +1,12 @@
+package first_api_test;
+
+import com.intuit.karate.junit5.Karate;
+
+class TestRunner {
+    @Karate.Test
+
+    public Karate curso() {
+        //Ruta del archivo feature
+        return Karate.run("classpath:first_api_test/FirstTest.feature");
+    }
+}
